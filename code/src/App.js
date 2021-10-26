654a21c1938243b25b6346a584b2e791d6d346f5
madeleinesvensson <56533906+madeleinesvensson@users.noreply.github.com>
2021-10-26 13:29:40 +0200

    input added to list of thoughts

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import moment from "moment";
+import { API_URL } from "./utils/urls";
 
 export const App = () => {
+  const [thoughts, setThoughts] = useState([]);
+  const [newThought, setNewThought] = useState("");
+  useEffect(() => {
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((data) => setThoughts(data));
+  }, []);
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ message: newThought }),
+    };
+
+    fetch(API_URL, options)
+      .then((res) => res.json())
+      .then((data) => setThoughts([data, ...thoughts]));
+  };
+
   return (
     <div>
-      Find me in src/app.js!
+      <form onSubmit={onFormSubmit}>
+        <label htmlFor="newThought">Type your text</label>
+        <input
+          type="text"
+          value={newThought}
+          onChange={(e) => setNewThought(e.target.value)}
+        />
+        <button type="submit">Send thought</button>
+      </form>
+      {thoughts.map((thought) => (
+        <div key={thought._id}>
+          <p>{thought.message}</p>
+          <button>{thought.hearts}</button>
+          <p>Created at: {moment(thought.createdAt).fromNow()}</p>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
